6782364cce29b2ab562afbf384c80f1dff953b8d
Nicola-Perera <nicola.perera96@mail.com>
2022-01-21 03:18:57 +0100

    create table to view posts resources

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -4,4 +4,31 @@
 
 <h1>All posts list</h1>
 
+<div class="container">
+    
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Title</th>
+                <th>Body</th>
+                <th>Create At</th>
+                <th>Updated At</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($posts as $post)
+            <tr>
+                <td scope="row">{{ $post->id }}</td>
+                <td>{{ $post->title }}</td>
+                <td>{{ $post->created_at }}</td>
+                <td>{{ $post->updated_at }}</td>
+                <td>View - edit - delete</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+
+</div>
 @endsection
