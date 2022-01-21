ecdd3eda6ebe5852a1c2a1cb3a73dc3929ca8f5b
Nicola-Perera <nicola.perera96@mail.com>
2022-01-21 02:37:44 +0100

    fix restful resources path with "\"

--- routes/web.php
+++ routes/web.php
@@ -25,22 +25,22 @@ Route::get('comics/{comic}', 'ComicController@show')->name('comic');
 // Posts by Admin
 
 // show list of all resources
-Route::get('posts', 'Admin/PostController@index')->name('admin.posts.index');
+Route::get('posts', 'Admin\PostController@index')->name('admin.posts.index');
 
 // show form to create new resource
-Route::get('posts/create', 'Admin/PostController@create')->name('admin.posts.create');
+Route::get('posts/create', 'Admin\PostController@create')->name('admin.posts.create');
 
 // save resource into database
-Route::post('posts', 'Admin/PostController@store')->name('admin.posts.store');
+Route::post('posts', 'Admin\PostController@store')->name('admin.posts.store');
 
 // show single resource
-Route::get('posts/{post}', 'Admin/PostController@show')->name('admin.posts.show');
+Route::get('posts/{post}', 'Admin\PostController@show')->name('admin.posts.show');
 
 // show form to edit resource
-Route::get('posts/{post}/edit', 'Admin/PostController@edit')->name('admin.posts.edit');
+Route::get('posts/{post}/edit', 'Admin\PostController@edit')->name('admin.posts.edit');
 
 // update the resource into database
-Route::put('posts/{posts}', 'Admin/PostController@update')->name('admin.posts.update');
+Route::put('posts/{posts}', 'Admin\PostController@update')->name('admin.posts.update');
 
 // delete resource
-Route::delete('posts/{posts}', 'Admin/PostController@destroy')->name('admin.posts.destroy');
+Route::delete('posts/{posts}', 'Admin\PostController@destroy')->name('admin.posts.destroy');
